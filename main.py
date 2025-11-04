ebc003c6f81761eedcfc0e509d540c08f115f73d
AnthonyW90 <anthonyt.wallace@icloud.com>
2025-11-04 10:40:46 -0800

    First attempts at tokenization

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
+import re
+
+
 def main():
-    print("Hello from large-language-model!")
+    with open("./the-verdict.txt", "r", encoding="utf-8") as file:
+        content = file.read()
+
+    preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', content)
+    preprocessed = [word.strip() for word in preprocessed if word.strip()]
+
+    all_words = sorted(set(preprocessed))
+    vocab_size = len(all_words)
+
+    vocab = {word: index + 1000 for index, word in enumerate(all_words)}
+
+    for i, item in enumerate(vocab.items()):
+        print(item)
+        if i >= 50:
+            break
 
 
 if __name__ == "__main__":
