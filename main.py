41bfc050a1814c3e14a706fa8cf9406f4b2c9c57
AnthonyW90 <anthonyt.wallace@icloud.com>
2025-11-04 11:31:36 -0800

    added tiktoken to handle more complex vocab

--- main.py
+++ main.py
@@ -1,30 +1,13 @@
-import re
-
-from Tokenizer import SimpleTokenizerV1, SimpleTokenizerV2
+import tiktoken
 
 
 def main():
     with open("./the-verdict.txt", "r", encoding="utf-8") as file:
         content = file.read()
 
-    preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', content)
-    preprocessed = [word.strip() for word in preprocessed if word.strip()]
-
-    all_words = sorted(set(preprocessed))
-    all_words.extend(["<|endoftext|>", "<|unk|>"])
-    vocab_size = len(all_words)
-
-    vocab = {word: index + 1000 for index, word in enumerate(all_words)}
-
-    text1 = "Hello, do you like tea?"
-    text2 = "In the sunlit terraces of the palace."
-
-    text = " <|endoftext|> ".join((text1, text2))
-
-    tokenizer = SimpleTokenizerV2(vocab)
-
-    ids = tokenizer.encode(text)
-    print(tokenizer.decode(ids))
+    tokenizer = tiktoken.get_encoding("gpt2")
+    tokens = tokenizer.encode(content)
+    print(tokens[:99])
 
 
 if __name__ == "__main__":
